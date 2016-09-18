f4ec3614e81d9ec4dd36d3d2671019b7992eeb8b
Consfuso <giovannilaperna@gmail.com>
2016-09-18 17:29:55 +0200

    rearrange folders structure

--- app.js
+++ app.js
@@ -2,46 +2,17 @@
 
 var express = require('express')
   , app = express()
-  , middleware = require('./purse_middleware')
-  , pi =  middleware.pi
-  , request = require('request')
-  , _ = require('underscore')
   , consolidate = require('consolidate')
+  , path = require('path')
 
 app.engine('html', consolidate.handlebars);
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
 
-app.get('/purse/all/:country', pi.all, pi.html, function (req, res) {
-  res.render( "purse_table", res.locals );
-});
+app.use('/purse', require('./purse/router'))
 
-app.get('/purse/fast/:country', pi.all, pi.fast, pi.html, function (req, res) {
-  res.render( "purse_table", res.locals );
-});
-
-app.get('/purse/api/all', pi.all, function (req, res) {
-    res.json(res.locals)
-  });
-
-app.get('/purse/api/all/bycountry', pi.all, pi.bycountry, function (req, res) {
-  res.json(res.locals)
-});
-
-app.get('/purse/api/all/:country', pi.all, function (req, res) {
-  res.json(res.locals)
-});
-
-app.get('/purse/api/fast', pi.all, pi.fast, pi.bycountry, function (req, res) {
-  res.json(res.locals)
-});
-
-app.get('/purse/api/fast/bycountry', pi.all, pi.fast, pi.bycountry, function (req, res) {
-  res.json(res.locals)
-});
-
-app.get('/purse/api/fast/:country', pi.all, pi.fast, function (req, res) {
-  res.json(res.locals)
+app.listen(3000, function () {
+  console.log('Listening on port 3000');
 });
 
 /*
@@ -89,7 +60,3 @@ app.get('/api/me', function (req, res, next) {
   });
 });
 */
-
-app.listen(3000, function () {
-  console.log('Listening on port 3000');
-});
